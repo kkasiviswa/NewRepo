869af77d8a957e7ce5da4a4c46d141f8767379f2
KISHORE TANGIRALA <KISHORE TANGIRALA>
2016-09-19 13:54:00 +0530

    main masiinn

--- main.java
+++ main.java
@@ -4,8 +4,18 @@ class main
 	{
 		System.out.println("Hello World!");
 
+
 System.out.println("hello hi");
 Syste.out.println("HI");
 System.out.println("hello hi");
-System.out.println("hello hi");	}
+System.out.println("hello hi");
+System.out.println("hello hi");
+Syste.out.println("HI");
+System.out.println("hello hi");
+System.out.println("hello hi");
+System.out.println("hello hi");
+Syste.out.println("HI");
+System.out.println("hello hi");
+System.out.println("hello hi");
+	}
 }
